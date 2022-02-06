137e5c8cae3d9073a7f320083ef126e8691f53c6
marcosteam <wdy71608161@gmail.com>
2022-02-06 10:44:04 +0800

    init nuxt3

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
